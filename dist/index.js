4b80f7355817e458c5782e49ec723239bacedce5
Julien Etienne <julien.etienne@intuiquest.com>
2024-04-02 10:42:14 +0700

    add firefox support

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,10 @@
 const manifest = require('../manifest.json');
 
-console.log(`zip dist/${manifest.version}.zip manifest.json icons/* scripts/*`);
+console.log(`cd dist/`);
+console.log(`cp -r ../manifest.json ../icons ../scripts .`);
+console.log(`zip ${manifest.version}-chrome.zip manifest.json icons/* scripts/*`);
+
+console.log(`cp ../manifest-firefox.json manifest.json`)
+console.log(`zip ${manifest.version}-firefox.zip manifest.json icons/* scripts/*`);
+
+console.log(`rm -rf manifest.json icons scripts`);
