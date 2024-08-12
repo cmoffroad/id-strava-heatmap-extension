d5defc59a49972d1fba4e784d573430342cca8a6
Julien Etienne <julien.etienne@intuiquest.com>
2024-08-12 15:55:11 +0700

    bump firefox version

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
-const manifest = require('../manifest.json');
-
+const chrome = require('../manifest.json');
 console.log(`cd dist/`);
 console.log(`cp -r ../manifest.json ../icons ../scripts .`);
-console.log(`zip ${manifest.version}-chrome.zip manifest.json icons/* scripts/*`);
+console.log(`zip ${chrome.version}-chrome.zip manifest.json icons/* scripts/*`);
 
+const firefox = require('../manifest-firefox.json');
 console.log(`cp ../manifest-firefox.json manifest.json`)
-console.log(`zip ${manifest.version}-firefox.zip manifest.json icons/* scripts/*`);
+console.log(`zip ${firefox.version}-firefox.zip manifest.json icons/* scripts/*`);
 
 console.log(`rm -rf manifest.json icons scripts`);
