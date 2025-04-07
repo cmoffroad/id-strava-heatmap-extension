d7eb44dfd3da595e9528704168433b5da00cf0be
Julien ETIENNE <julien.etienne@intuiquest.com>
2025-04-07 09:41:00 +0700

    add missing await

--- src/content/reset-credentials.js
+++ src/content/reset-credentials.js
@@ -1,7 +1,7 @@
 async function resetCredentials() {
 	try {
 		// Send message to reset credentials
-		browser.runtime.sendMessage('resetCredentials');
+		await browser.runtime.sendMessage('resetCredentials');
 		console.log('Credentials reset successfully.');
 	} catch (error) {
 		console.error('Error resetting credentials:', error);
